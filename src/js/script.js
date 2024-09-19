04dc603222083dad866313a615625b539719825f
Jose Pardo <jose.pardo@upch.pe>
2024-09-19 10:59:20 -0500

    update10591909

--- src/js/script.js
+++ src/js/script.js
@@ -200,6 +200,93 @@ window.addEventListener("load", () => {
 
    })
 
+
+
+    function getUTMParams() {
+        var params = {};
+        var queryString = window.location.search;
+        queryString = queryString.substring(1);
+        var pairs = queryString.split("&");
+        for (var i = 0; i < pairs.length; i++) {
+            var pair = pairs[i].split("=");
+            params[pair[0]] = pair[1];
+        }
+        return params;
+    }
+
+    var utmParams = getUTMParams();
+    var utmSource = utmParams["utm_source"];
+    var utmMedium = utmParams["utm_medium"];
+    var utmCampaign = utmParams["utm_campaign"];
+    var utmTerm = utmParams["utm_term"];
+    var utmContent = utmParams["utm_content"];
+    var utmId = utmParams["utm_id"];
+
+    let adm = document.querySelector('.administracion');
+    let adm_salud = document.querySelector('.administracionEnSalud');
+    let medicina = document.querySelector('.medicina');
+    let enfermeria = document.querySelector('.enfermeria');
+    let estomatologia = document.querySelector('.estomatologia');
+    let psicologia = document.querySelector('.psicologia');
+    let veterinaria = document.querySelector('.veterinaria');
+    let tfr = document.querySelector('.tfr');
+    let to = document.querySelector('.to');
+    let tavl = document.querySelector('.tavl');
+    let radiologia = document.querySelector('.radiologia');
+    let laboratorio = document.querySelector('.laboratorio');
+    let industrial = document.querySelector('.industrial');
+    let ambiental = document.querySelector('.ambiental');
+    let informatica = document.querySelector('.informatica');
+    let biomedica = document.querySelector('.biomedica');
+    let edu = document.querySelector('.educacion');
+    let biologia = document.querySelector('.biologia');
+    let nutricion = document.querySelector('.nutricion');
+    let farmacia = document.querySelector('.farmacia');
+
+    if( !utmCampaign ) {
+        adm.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/administracion/`, '_blank')")
+        adm_salud.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/administracion-en-salud`, '_blank')")
+        medicina.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/medicina/`, '_blank')")
+        enfermeria.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/enfermeria/`, '_blank')")
+        estomatologia.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/estomatologia/`, '_blank')")
+        psicologia.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/psicologia/`, '_blank')")
+        veterinaria.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/medicina-veterinaria-y-zootecnia/`, '_blank')")
+        tfr.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/terapia-fisica-y-rehabilitacion`, '_blank')")
+        to.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/terapia-ocupacional/`, '_blank')")
+        tavl.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/terapia-de-audicion-voz-y-lenguaje`, '_blank')")
+        radiologia.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/radiologia/`, '_blank')")
+        laboratorio.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/laboratorio-clinico/`, '_blank')")
+        industrial.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/ingenieria-industrial/`, '_blank')")
+        ambiental.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/ingenieria-ambiental/`, '_blank')")
+        informatica.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/ingenieria-informatica/`, '_blank')")
+        biomedica.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/ingenieria-biomedica/`, '_blank')")
+        edu.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/educacion/`, '_blank')")
+        biologia.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/biologia/`, '_blank')")
+        nutricion.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/nutricion/`, '_blank')")
+        farmacia.setAttribute("onclick", "window.open(`https://lp.cayetano.edu.pe/farmacia-y-bioquimica/`, '_blank')")
+    }else {
+        adm.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/administracion/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        adm_salud.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/administracion-en-salud/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        medicina.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/medicina/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        enfermeria.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/enfermeria/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        estomatologia.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/estomatologia/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        psicologia.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/psicologia/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        veterinaria.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/medicina-veterinaria-y-zootecnia/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        tfr.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/terapia-fisica-y-rehabilitacion/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        to.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/terapia-ocupacional/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        tavl.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/terapia-de-audicion-voz-y-lenguaje/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        radiologia.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/radiologia/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        laboratorio.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/laboratorio-clinico/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        industrial.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/ingenieria-industrial/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        ambiental.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/ingenieria-ambiental/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        informatica.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/ingenieria-informatica/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        biomedica.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/ingenieria-biomedica/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        edu.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/educacion/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        biologia.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/biologia/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        nutricion.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/nutricion/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+        farmacia.setAttribute("onclick", `window.open('https://lp.cayetano.edu.pe/farmacia-y-bioquimica/?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}&utm_id=${utmId}', '_blank')`)
+    }
+
 })
 
 
